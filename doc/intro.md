fd9fbb0fb9486932a56f20e0375b779385fd15a1
Harri Lehtola <peruukki@hotmail.com>
2015-10-05 23:29:41 +0200

    Project skeleton created with 'lein new app nhl-score-api'

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to nhl-score-api
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/nhl_score_api/core.clj
@@ -0,0 +1,7 @@
+(ns nhl-score-api.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
