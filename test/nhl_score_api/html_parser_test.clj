c35698808cdae592dab5c75d8406fd4b465d5fe5
Harri Lehtola <peruukki@hotmail.com>
2015-10-08 20:57:54 +0200

    Use more concise dot notation instead of 'new' keyword

--- test/nhl_score_api/html_parser_test.clj
+++ test/nhl_score_api/html_parser_test.clj
@@ -5,8 +5,8 @@
 
 (def resource-path "test/nhl_score_api/resources/")
 
-(def html-with-games (new File (str resource-path "scores-2015-10-03.htm")))
-(def html-without-games (new File (str resource-path "scores-2015-10-06.htm")))
+(def html-with-games (File. (str resource-path "scores-2015-10-03.htm")))
+(def html-without-games (File. (str resource-path "scores-2015-10-06.htm")))
 
 (deftest game-score-html-parsing
 
