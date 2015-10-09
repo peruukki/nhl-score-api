ae143b5938792520ee26a9acf63a684db3e6be14
Harri Lehtola <peruukki@hotmail.com>
2015-10-10 01:01:14 +0200

    Report project version as default response

--- src/nhl_score_api/core.clj
+++ src/nhl_score_api/core.clj
@@ -4,20 +4,22 @@
             [org.httpkit.server :as server]
             [clojure.data.json :as json]))
 
+(def version (System/getProperty "nhl-score-api.version"))
+
 (declare app)
 
 (defn -main [& args]
   (let [ip (get (System/getenv) "OPENSHIFT_CLOJURE_HTTP_IP" "0.0.0.0")
         port (Integer/parseInt (get (System/getenv) "OPENSHIFT_CLOJURE_HTTP_PORT" "8080"))]
-    (println "Starting server, listening on" (str ip ":" port))
+    (println "Starting server version" (str version ",") "listening on" (str ip ":" port))
     (server/run-server app {:ip ip :port port})))
 
-(defn get-response [request-path]
+(defn- get-response [request-path]
   (case request-path
     "/api/scores/latest"
     (api/fetch-latest-scores)
 
-    {}))
+    {:version version}))
 
 (defn app [req]
   (println "Received request" req)
