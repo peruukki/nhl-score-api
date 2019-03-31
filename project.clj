8baef50f05e01d17711e09e4edc9c2ac0b488111
Harri Lehtola <peruukki@hotmail.com>
2019-03-31 17:57:38 +0300

    Update Clojure version 1.7.0 -> 1.10.0
    This also requires updating com.taoensso/carmine version 2.12.0 -> 2.19.1.

--- project.clj
+++ project.clj
@@ -2,12 +2,12 @@
   :description "A JSON API that returns the scores and goals from the latest finished NHL games."
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/data.json "0.2.6"]
                  [clj-http-lite "0.3.0"] ; clj-http-lite supports SNI (unlike http-kit or clj-http)
                  [clj-time "0.11.0"]
                  [camel-snake-kebab "0.3.2"]
-                 [com.taoensso/carmine "2.12.0"]
+                 [com.taoensso/carmine "2.19.1"]
                  [enlive "1.1.6"]
                  [http-kit "2.1.19"]
                  [yleisradio/new-reliquary "1.0.1"]]
