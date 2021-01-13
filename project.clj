a877159ded81242c99af81410b3afd1d2dc9b517
Harri Lehtola <peruukki@hotmail.com>
2021-01-13 21:24:24 +0200

    Set :min-lein-version to 2.0.0
    Saw a warning about this missing when deploying to a new Heroku stack.

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "A JSON API that returns the scores and goals from the latest finished NHL games."
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/data.json "0.2.6"]
                  [clj-http-lite "0.3.0"] ; clj-http-lite supports SNI (unlike http-kit or clj-http)
