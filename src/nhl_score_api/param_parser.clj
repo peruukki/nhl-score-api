98db6736cc8b0ff862f1c7bcb786255f55013012
Harri Lehtola <peruukki@hotmail.com>
2021-10-04 22:23:42 +0300

    Add request parameter parser

---
+++ src/nhl_score_api/param_parser.clj
@@ -0,0 +1,29 @@
+(ns nhl-score-api.param-parser
+  (:require [clj-time.format :as format]
+            [camel-snake-kebab.core :refer [->camelCaseString]]))
+
+(defn- parse-fn-date [name field value]
+  (try
+    {:success {field (format/parse (format/formatters :year-month-day) value)}}
+    (catch Exception e {:error (str "Invalid parameter " name ": " (.getMessage e))})))
+
+(def parse-fns {:date parse-fn-date})
+
+(defn- parse-param [field value type required?]
+  (let [name (->camelCaseString field)
+        parse-fn (type parse-fns)]
+    (cond (and required? (nil? value))
+          {:error (str "Missing required parameter " name)}
+          (nil? value)
+          {:success {field value}}
+          :else
+          (parse-fn name field value))))
+
+(defn parse-params [params-to-parse request-params]
+  (let [parse-results
+        (map #(parse-param (:field %) ((:field %) request-params) (:type %) (:required? %))
+             params-to-parse)
+        parsed-values (filter some? (map #(:success %) parse-results))
+        parsing-errors (filter some? (map #(:error %) parse-results))]
+    {:values (apply merge parsed-values)
+     :errors parsing-errors}))
---
+++ test/nhl_score_api/param_parser_test.clj
@@ -0,0 +1,43 @@
+(ns nhl-score-api.param-parser-test
+  (:require [clojure.test :refer :all]
+            [nhl-score-api.param-parser :refer :all]
+            [clj-time.core :as time]))
+
+(deftest parsing-date-param
+  (testing "Parsing valid date"
+    (is (= {:values {:date (time/date-time 2021 7 30)}
+            :errors []}
+           (parse-params [{:field :date :type :date}] {:date "2021-07-30"}))
+        "Success result with parsed date"))
+
+  (testing "Parsing multiple valid dates"
+    (is (= {:values {:start-date (time/date-time 2021 7 30)
+                     :end-date (time/date-time 2021 7 31)}
+            :errors []}
+           (parse-params [{:field :start-date :type :date} {:field :end-date :type :date}]
+                         {:start-date "2021-07-30" :end-date "2021-07-31"}))
+        "Success result with parsed dates"))
+
+  (testing "Parsing invalid date value"
+    (is (= {:values nil
+            :errors ["Invalid parameter date: Cannot parse \"2021-07-0\": Value 0 for dayOfMonth must be in the range [1,31]"]}
+           (parse-params [{:field :date :type :date}] {:date "2021-07-0"}))
+        "Invalid parameter error"))
+
+  (testing "Parsing totally invalid value"
+    (is (= {:values nil
+            :errors ["Invalid parameter date: Invalid format: \"whatisdis\""]}
+           (parse-params [{:field :date :type :date}] {:date "whatisdis"}))
+        "Invalid parameter error"))
+
+  (testing "Parsing optional missing date"
+    (is (= {:values {:date nil}
+            :errors []}
+           (parse-params [{:field :date :type :date}] {}))
+        "Success result with nil date"))
+
+  (testing "Parsing required missing date"
+    (is (= {:values nil
+            :errors ["Missing required parameter date"]}
+           (parse-params [{:field :date :required? true :type :date}] {}))
+        "Missing value error")))
