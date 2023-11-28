df09f866d8a1e5ce530e51e59ef1eb5b76322792
Harri Lehtola <peruukki@hotmail.com>
2023-11-28 08:49:03 +0200

    Version 0.47.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nhl-score-api "0.46.1"
+(defproject nhl-score-api "0.47.0"
   :description "A JSON API that returns the scores and goals from the latest finished or on-going NHL games."
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
