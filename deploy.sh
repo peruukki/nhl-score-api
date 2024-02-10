3794d2d4681e00d7a59bd5d9b050562e3a90351d
Harri Lehtola <peruukki@hotmail.com>
2024-02-10 10:58:07 +0200

    Check for missing newrelic.jar in deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+if [ ! -f newrelic/newrelic.jar ]; then
+  echo "newrelic/newrelic.jar is missing; set up New Relic and copy downloaded New Relic files into the newrelic directory"
+  exit 1
+fi
+
 lein uberjar && heroku deploy:jar --includes newrelic target/server.jar
