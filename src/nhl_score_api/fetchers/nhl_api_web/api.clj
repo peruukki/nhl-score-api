fcf22c379cb7b1d6452bfb7151bf40f930cd68bd
Harri Lehtola <peruukki@hotmail.com>
2024-10-06 09:35:09 +0300

    Reformat api.clj

--- src/nhl_score_api/fetchers/nhl_api_web/api.clj
+++ src/nhl_score_api/fetchers/nhl_api_web/api.clj
@@ -43,20 +43,20 @@
 (defrecord StandingsApiRequest [date-str schedule-response pre-game-standings-response]
   ApiRequest
   (archive? [_ response]
-            (let [games (get-games-in-date-range date-str nil schedule-response)
-                  all-games-in-official-state? (every? #(= "OFF" (:game-state %)) games)]
-              (if (and all-games-in-official-state? pre-game-standings-response)
-                (->> games
-                     (map #(seq [(get-in % [:away-team :abbrev])
-                                 (get-in % [:home-team :abbrev])]))
-                     flatten
-                     (every? #(let [pre-game-standings (get-team-standings % pre-game-standings-response)
-                                    current-standings (get-team-standings % response)]
-                                (and pre-game-standings
-                                     current-standings
-                                     (> (:games-played current-standings)
-                                        (:games-played pre-game-standings))))))
-                all-games-in-official-state?)))
+    (let [games (get-games-in-date-range date-str nil schedule-response)
+          all-games-in-official-state? (every? #(= "OFF" (:game-state %)) games)]
+      (if (and all-games-in-official-state? pre-game-standings-response)
+        (->> games
+             (map #(seq [(get-in % [:away-team :abbrev])
+                         (get-in % [:home-team :abbrev])]))
+             flatten
+             (every? #(let [pre-game-standings (get-team-standings % pre-game-standings-response)
+                            current-standings (get-team-standings % response)]
+                        (and pre-game-standings
+                             current-standings
+                             (> (:games-played current-standings)
+                                (:games-played pre-game-standings))))))
+        all-games-in-official-state?)))
   (cache-key [_] (str "standings-" date-str))
   (description [_] (str "standings " {:date date-str}))
   (url [_] (str base-url "/standings/" date-str)))
