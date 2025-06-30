a59ca8fa584cf52ea0c81086e9b1de8b90c025b1
Harri Lehtola <peruukki@hotmail.com>
2025-06-30 13:35:16 +0300

    Add more fields to schedule API response schema

--- src/nhl_score_api/fetchers/nhl_api_web/api/schedule.clj
+++ src/nhl_score_api/fetchers/nhl_api_web/api/schedule.clj
@@ -1,14 +1,46 @@
 (ns nhl-score-api.fetchers.nhl-api-web.api.schedule
   (:require [malli.core :as malli]
-            [nhl-score-api.fetchers.nhl-api-web.api.index :as api]))
+            [nhl-score-api.fetchers.nhl-api-web.api.index :as api]
+            [nhl-score-api.fetchers.nhl-api-web.api.schema :as schema]))
+
+(def PeriodTypeSchema
+  (malli/schema
+   [:enum "OT" "REG" "SO"]))
+
+(def TeamSchema
+  (malli/schema
+   [:map
+    [:abbrev :string]
+    [:id :int]
+    [:place-name #'schema/Localized]
+    [:score {:optional true} :int]]))
 
 (def GameSchema
   (malli/schema
    [:map
+    [:away-team #'TeamSchema]
     [:id :int]
+    [:game-center-link {:optional true} :string]
+    [:game-outcome {:optional true}
+     [:map
+      [:last-period-type #'PeriodTypeSchema]]]
     [:game-schedule-state [:enum "OK" "PPD"]]
     [:game-state [:enum "CRIT" "FINAL" "FUT" "LIVE" "OFF" "OVER" "PRE"]]
-    [:game-type :int]]))
+    [:game-type :int]
+    [:home-team #'TeamSchema]
+    [:period-descriptor
+     [:map
+      [:number {:optional true} :int]
+      [:period-type {:optional true} #'PeriodTypeSchema]]]
+    [:series-url {:optional true} :string]
+    [:series-status {:optional true}
+     [:map
+      [:bottom-seed-wins :int]
+      [:game-number-of-series :int]
+      [:round :int]
+      [:top-seed-wins :int]]]
+    [:start-time-utc :string]
+    [:three-min-recap {:optional true} :string]]))
 
 (def GameWeekSchema
   (malli/schema
